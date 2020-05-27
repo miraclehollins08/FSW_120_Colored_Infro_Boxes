2a1179a428ae188fcc321c57a7cbefdba4035dc3
Miracle <54377927+miraclehollins08@users.noreply.github.com>
2020-05-26 17:24:49 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+    render(){
+        return (
+            <div>
+
+          
+                
+                <h1>Super-Man</h1>
+                <backgroundcolor><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL9swb_Y5nMLROGBsxKj5SUufTb23YywDQMr6skmMi8VGpQM4d-RbIPx8U&amp;s" alt="Superman Returns Lex Luthor Lois Lane Brandon Routh - Superman PNG ..."/></backgroundcolor>
+                <p>I am Superman!</p>
+
+                
+                <h2>AquaMan</h2>
+                <background><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_hf8wRYHIrpKtUBBsn7aSzXYheWdADHcYwOqSysBXVJYSsHal3EikmsQ&amp;s" alt="Aquaman Atlanna Batman King Atlan Superman - aquaman png download ..."/></background>
+                <p>I am The King of all Sea!</p>
+
+                
+                <h3>BatMan</h3>
+                <background><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmJtMeTFr_VZbvDOg2Wt16VYRtKPFVgAhZBFqHmb38Wpe3HO1lIbBbT591&amp;s" alt="Batman Diana Prince Cyborg Mera DeviantArt - batman png download ..."/></background>
+                <p>Joker I will find you</p>
+
+                
+                <h4>The Flash</h4>
+                <background><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9DZ5pJAtjToU4OAwM8sf77CFKeIbnThUo9AHqQlcoG7_w-5X8W2szzgyf&amp;s" alt="The Flash - Season 4 Mixed Signals Grant Gustin Iris West Allen ..."/></background>
+                <p>Who faster than the Flash </p>
+            </div>
+
+        )
+    }
+}
+
+export default App
